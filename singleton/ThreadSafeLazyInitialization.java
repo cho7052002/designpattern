8042412bec237762c3f18ab0a1fbf81686915bc0
cho7052002 <36180935+cho7052002@users.noreply.github.com>
2019-04-23 00:53:59 +0900

    Update ThreadSafeLazyInitialization.java

--- singleton/ThreadSafeLazyInitialization.java
+++ singleton/ThreadSafeLazyInitialization.java
@@ -1,15 +1,15 @@
-public class ThreadSafeLazyInitialization{
+public class Singleton{
 
 	//static 싱글턴 인스턴스
-	private static ThreadSafeLazyInitialization instance;
+	private static Singleton instance;
 	
 	//생성자를 private로 설정, 외부에서 호출 불가능하도록
-	private ThreadSafeLazyInitialization(){}
+	private Singleton(){}
 	
 	//synchronized인 get instance 를 호출해 인스턴스 
-	public static synchronized ThreadSafeLazyInitialization getInstance(){
+	public static synchronized Singleton getInstance(){
 		if(instance == null) {
-			instance = new ThreadSafeLazyInitialization();
+			instance = new Singleton();
 		}
 		return instance;
 	}
