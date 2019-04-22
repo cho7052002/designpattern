50164b7af37b737e9346639727da3a2c1cdd20f4
cho7052002 <36180935+cho7052002@users.noreply.github.com>
2019-04-23 02:15:59 +0900

    Create EarlyInitialization.java

---
+++ singleton/EarlyInitialization.java
@@ -0,0 +1,10 @@
+//기본적인 
+public class Singleton {
+	private static final Singleton instance = new Singleton();
+	
+	private Singleton(){}
+	
+	public static Singleton getInstance() {
+		return instance;
+	}
+}
