8bca77942e27062f754e0f000d687e31b8898d05
cho7052002 <36180935+cho7052002@users.noreply.github.com>
2019-04-24 01:53:54 +0900

    Create MyServiceImpl.java

---
+++ Proxy/MyServiceImpl.java
@@ -0,0 +1,11 @@
+public class MyServiceImpl implements MyService {
+    @Override
+    public int getNum(int a) {
+        return a;
+    }
+
+    @Override
+    public String getStr(String str) {
+        return str;
+    }
+}
